85842e1f262b8241333a331d37c2cfb70ba877d5
Harold Martin <harold.martin@gmail.com>
2026-07-19 05:24:53 -0700

    Hard-fail libgit2 initialization errors

--- ObjectiveGit/ObjectiveGit.m
+++ ObjectiveGit/ObjectiveGit.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <os/log.h>
+#include <stdlib.h>
 
 #import "git2/common.h"
 #import "git2/global.h"
@@ -21,20 +22,13 @@ static os_log_t GTLog(void) {
 	return log;
 }
 
-// YES once git_libgit2_init has succeeded; libgit2 calls made while this is NO
-// are unsafe.
-static BOOL GTLibgit2Initialized = NO;
-
 __attribute__((constructor))
 static void GTSetup(void) {
 	int initResult = git_libgit2_init();
 	if (initResult < 0) {
 		os_log_error(GTLog(), "ObjectiveGit failed to initialize libgit2: %d", initResult);
-		NSCAssert(NO, @"git_libgit2_init failed: %d", initResult);
-		return;
+		abort();
 	}
-
-	GTLibgit2Initialized = YES;
 
 	int major = 0, minor = 0, rev = 0;
 	git_libgit2_version(&major, &minor, &rev);
